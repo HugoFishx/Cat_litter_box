16ee7660797493f9fbea2c5197f46438c85aba3e
shiqiyu2 <shiqiyu2@illinois.edu>
2021-10-12 02:05:31 -0400

    Arduino motion sensor handler

--- cat_litter_box/cat_litter_box.ino
+++ cat_litter_box/cat_litter_box.ino
@@ -1,35 +1,55 @@
-#define MOTION_SENSOR_PIN 1
+/*-----------------includes-----------------*/
 
-// global variables
+/*-----------------define-----------------*/
+#define MOTION_SENSOR_PIN 2 // only 2, 3 can be interrupts
+
+/*-----------------global var-----------------*/
 unsigned long start_time;
 unsigned long end_time;
 
 void setup() {
-  // init peripherals: wifi, RFID reader, motion sensor
+  /***init peripherals: wifi, RFID reader, motion sensor, SD card***/
 
-  // set pin mode
+  /***set pin mode***/
   pinMode(MOTION_SENSOR_PIN, INPUT);
-  // set interrupt
+  /***set interrupt***/
+  // attachInterrupt(digitalPinToInterrupt(MOTION_SENSOR_PIN), test_motion_sensor, CHANGE);
   attachInterrupt(digitalPinToInterrupt(MOTION_SENSOR_PIN), motion_sensor_handler, CHANGE);
+  /***misc***/
+  Serial.begin(9600);
 }
 
 void loop() {
-
 }
 
+/*-----------------functions-----------------*/
 void motion_sensor_handler() {
   if (digitalRead(MOTION_SENSOR_PIN)) { // check whether enter or leave
-    // enter handler
+    /***enter handler***/
     RFID_read();
     start_time = millis(); // can also record time in real word. may need internet
   } else {
-    // leave handler
+    /***leave handler***/
     end_time = millis();
-    
+    SDcard_store();
   }
   return;
 }
 
+void test_motion_sensor() {
+  if (digitalRead(MOTION_SENSOR_PIN)) {
+    Serial.println("Start!");
+    start_time = millis();
+  } else {
+    end_time = millis();
+    Serial.println((end_time-start_time)/1000);
+  }
+}
+
 int RFID_read() {
   return 0;
+}
+
+void SDcard_store() {
+  return;
 }
